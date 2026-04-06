a2e62bef1bb2d7621ea5544514a4fe9698b8636d
Bruno Cézar Tenório Lima <btenorio@granderecife.inet>
2026-04-06 11:51:41 -0300

    atualizando

--- script.js
+++ script.js
@@ -4,56 +4,59 @@ const campoEmail = document.querySelector(".campo_email");
 const campoCelular = document.querySelector(".campo_celular");
 const valores = document.querySelector(".valores");
 const feedback = document.createElement("div");
-function exibirNome() {
-  const nome = document.createElement("div")
-  const nomeLabel = document.createElement("span")
-  nomeLabel.textContent = "Nome: "
-  nomeLabel.style.fontWeight = "bold"
-  const nomeValor = document.createElement("span")
-  nomeValor.textContent = campoNome.value
-  valores.appendChild(nome)
-  nome.appendChild(nomeLabel)
-  nome.appendChild(nomeValor)
-}
-function exibirEmail() {
-  const email = document.createElement("div")
-  const emailLabel = document.createElement("span")
-  emailLabel.textContent = "Email: "
-  emailLabel.style.fontWeight = "bold"
-  const emailValor = document.createElement("span")
-  emailValor.textContent = campoEmail.value
-  valores.appendChild(email)
-  email.appendChild(emailLabel)
-  email.appendChild(emailValor)
-}
-function exibirCelular() {
-  const celular = document.createElement("div")
-  const celularLabel = document.createElement("span")
-  celularLabel.textContent = "Celular: "
-  celularLabel.style.fontWeight = "bold"
-  const celularValor = document.createElement("span")
-  celularValor.textContent = campoCelular.value
-  valores.appendChild(celular)
-  celular.appendChild(celularLabel)
-  celular.appendChild(celularValor)
-  apagarValores()
-}
 function feedbackErro() {
   feedback.textContent = "Preencha todos os campos";
   feedback.classList.add("invalid-feedback", "feedback");
   valores.appendChild(feedback);
   campoNome.classList.add();
 }
+function exibirNome(campo) {
+  const nome = document.createElement("div");
+  const nomeLabel = document.createElement("span");
+  nomeLabel.textContent = "Nome: ";
+  nomeLabel.style.fontWeight = "bold";
+  const nomeValor = document.createElement("span");
+  nomeValor.textContent = campoNome.value;
+  campo.appendChild(nome);
+  nome.appendChild(nomeLabel);
+  nome.appendChild(nomeValor);
+}
+function exibirEmail(campo) {
+  const email = document.createElement("div");
+  const emailLabel = document.createElement("span");
+  emailLabel.textContent = "Email: ";
+  emailLabel.style.fontWeight = "bold";
+  const emailValor = document.createElement("span");
+  emailValor.textContent = campoEmail.value;
+  campo.appendChild(email);
+  email.appendChild(emailLabel);
+  email.appendChild(emailValor);
+}
+function exibirCelular(campo) {
+  const celular = document.createElement("div");
+  const celularLabel = document.createElement("span");
+  celularLabel.textContent = "Celular: ";
+  celularLabel.style.fontWeight = "bold";
+  const celularValor = document.createElement("span");
+  celularValor.textContent = campoCelular.value;
+  campo.appendChild(celular);
+  celular.appendChild(celularLabel);
+  celular.appendChild(celularValor);
+  apagarValores();
+}
 function apagarValores() {
-  campoNome.value = ""
-  campoEmail.value = ""
-  campoCelular.value = ""
+  campoNome.value = "";
+  campoEmail.value = "";
+  campoCelular.value = "";
 }
 adicionar.addEventListener("click", () => {
   if (campoNome.value && campoEmail.value && campoCelular.value) {
-    exibirNome();
-    exibirEmail();
-    exibirCelular();
+    const campo = document.createElement("div");
+    campo.classList.add("campo");
+    valores.appendChild(campo)
+    exibirNome(campo);
+    exibirEmail(campo);
+    exibirCelular(campo);
     feedback.remove();
   } else if (!valores.firstElementChild) {
     feedbackErro();
