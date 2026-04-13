169b7cacecbaa6b6e783dd7dfda243582fa6a327
Bruno Lima <bruuno.lima03@gmail.com>
2026-04-13 08:06:55 -0300

    atualizar

--- script.js
+++ script.js
@@ -1,56 +1,57 @@
-const adicionar = document.querySelector(".adicionar");
 const campoNome = document.querySelector(".campo_nome");
 const campoEmail = document.querySelector(".campo_email");
 const campoCelular = document.querySelector(".campo_celular");
+const adicionar = document.querySelector(".adicionar");
 const campos = document.querySelector(".campos");
 const feedback = document.createElement("div");
 function criarCampo() {
-  const campo = document.createElement("div")
-  campo.classList.add("campo")
-  campos.appendChild(campo)
-  const container = document.createElement("div")
-  campo.appendChild(container)
-  criarCampoItem(container, "Nome: ", campoNome.value)
-  criarCampoItem(container, "Email: ", campoEmail.value)
-  criarCampoItem(container, "Celular: ", campoCelular.value)
-  limparDados()
-  botaoRemover(campo)
-}
-function criarCampoItem(container, label, valor) {
-  const wrapper = document.createElement("div")
-  container.appendChild(wrapper)
-  const labelItem = document.createElement("span")
-  labelItem.textContent = `${label}`
-  labelItem.style.fontWeight = "bold"
-  wrapper.appendChild(labelItem)
-  const valorItem = document.createElement("span")
-  valorItem.textContent = `${valor}`
-  wrapper.appendChild(valorItem)
-}
-function limparDados() {
-  campoNome.value = ""
-  campoEmail.value = ""
-  campoCelular.value = ""
-}
-function botaoRemover(campo) {
-  const remover = document.createElement("button")
-  remover.classList.add("btn", "btn-danger", "remover")
-  remover.textContent = "X"
-  campo.appendChild(remover)
-  remover.addEventListener("click", () => {
-    campo.remove()
-  })
+  const campo = document.createElement("div");
+  campo.classList.add("campo");
+  campos.appendChild(campo);
+  const container = document.createElement("div");
+  container.classList.add("container");
+  campo.appendChild(container);
+  botaoRemover(campo);
+  adicionarValores(container, "Nome: ", campoNome.value);
+  adicionarValores(container, "Email: ", campoEmail.value);
+  adicionarValores(container, "Celular: ", campoCelular.value);
 }
 function feedbackErro() {
-  feedback.textContent = "Preencha todos os campos"
-  feedback.classList.add("invalid-feedback", "feedback")
-  campos.prepend(feedback)
+  feedback.textContent = "Preencha todos os campos";
+  feedback.classList.add("invalid-feedback", "feedback");
+  campos.prepend(feedback);
+}
+function limparDados() {
+  campoNome.value = "";
+  campoEmail.value = "";
+  campoCelular.value = "";
+}
+function botaoRemover(campo) {
+  const remover = document.createElement("button");
+  remover.textContent = "X";
+  remover.classList.add("btn", "btn-danger", "remover");
+  campo.appendChild(remover);
+  remover.addEventListener("click", () => {
+    campo.remove();
+  });
+}
+function adicionarValores(container, campoLabel, campoValor) {
+  const wrapper = document.createElement("div");
+  container.appendChild(wrapper);
+  const label = document.createElement("span");
+  label.textContent = campoLabel;
+  label.style.fontWeight = "bold";
+  wrapper.appendChild(label);
+  const valor = document.createElement("span");
+  valor.textContent = campoValor;
+  wrapper.appendChild(valor);
 }
 adicionar.addEventListener("click", () => {
   if (campoNome.value && campoEmail.value && campoCelular.value) {
-    feedback.remove()
-    criarCampo()
-  } else {
-    feedbackErro()
+    feedback.remove();
+    criarCampo();
+    limparDados();
+  } else if (!campoNome.value || campoEmail.value || campoCelular.value) {
+    feedbackErro();
   }
-})
+});
